c188e5c4122a19cd84dc8179094962d5ee4450cc
pranushreddy <157949203+pranush-reddy@users.noreply.github.com>
2024-05-29 19:19:36 +0530

    Update List.jsx

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
-function List(props) {
+function List({ id, text, completed, strike }) {
   return (
-    <div id="l" onClick={() => props.strike(props.id)}>
-      <li>{props.text}</li>
-    </div>
+    <div id="l">
+    <li
+      id={`item-${id}`}
+      style={{ 
+        textDecoration: completed ? "line-through" : "none" }}
+      onClick={() => strike(id)}
+    >
+      {text}
+    </li></div>
   );
 }
 
