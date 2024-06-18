6e53c3c3a84d8466fe0f826b924407d721cf58b6
pranushreddy <157949203+pranush-reddy@users.noreply.github.com>
2024-06-18 23:51:32 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,5 @@
 import React, { useState } from "react";
-<<<<<<< HEAD
-import List from "./List"; // Ensure the path to your List component is correct
-=======
 import List from "./List";
->>>>>>> 4c986bc07b96da5b56735f7b439b9d7060fa08b3
 import "./App.css";
 import AddIcon from '@mui/icons-material/Add';
 
@@ -17,28 +13,14 @@ function App() {
 
   function addItem(event) {
     event.preventDefault();
-<<<<<<< HEAD
     if (text !== "") {
       setItems((prevItems) => [...prevItems, { text: text }]);
-=======
-    if (text) {
-      setItems((prevItems) => [...prevItems, { text, completed: false }]);
->>>>>>> 4c986bc07b96da5b56735f7b439b9d7060fa08b3
       setText("");
     }
   }
 
-<<<<<<< HEAD
   function deleteItem(id) {
     setItems((prevItems) => prevItems.filter((item, index) => index !== id));
-=======
-  function strike(id) {
-    setItems((prevItems) =>
-      prevItems.map((item, index) =>
-        index === id ? { ...item, completed: !item.completed } : item
-      )
-    );
->>>>>>> 4c986bc07b96da5b56735f7b439b9d7060fa08b3
   }
 
   return (
@@ -53,17 +35,9 @@ function App() {
         </form>
       </div>
       <div id="content">
-<<<<<<< HEAD
         {items.map((el, index) => (
           <List key={index} id={index} text={el.text} onDelete={deleteItem} />
         ))}
-=======
-        <ul>
-          {items.map((el, index) => (
-            <List key={index} id={index} text={el.text} completed={el.completed} strike={strike} />
-          ))}
-        </ul>
->>>>>>> 4c986bc07b96da5b56735f7b439b9d7060fa08b3
       </div>
     </div>
   );
