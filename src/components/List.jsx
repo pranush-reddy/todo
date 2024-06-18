1174d3adaba549cd64c601e00d37b0735b17a83b
pranush-reddy <pranush.reddy0102@gmail.com>
2024-06-18 23:47:31 +0530

    Remove head

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import DeleteIcon from '@mui/icons-material/Delete';
 function List({ id, text , onDelete}) {
   return (
@@ -9,20 +8,6 @@ function List({ id, text , onDelete}) {
     <button id="del" onClick={() => onDelete(id)}>
       <DeleteIcon/>
       </button></div>
-=======
-
-function List({ id, text, completed, strike }) {
-  return (
-    <div id="l">
-    <li
-      id={`item-${id}`}
-      style={{ 
-        textDecoration: completed ? "line-through" : "none" }}
-      onClick={() => strike(id)}
-    >
-      {text}
-    </li></div>
->>>>>>> 4c986bc07b96da5b56735f7b439b9d7060fa08b3
   );
 }
 
