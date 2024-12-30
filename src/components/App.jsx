75028abedc3da7d196b81d261d10847441273b3d
pranushreddy <157949203+pranush-reddy@users.noreply.github.com>
2024-12-30 19:37:13 +0530

    implemented local storage and streak

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import List from "./List";
 import "./App.css";
 import AddIcon from '@mui/icons-material/Add';
@@ -6,6 +6,17 @@ import AddIcon from '@mui/icons-material/Add';
 function App() {
   const [text, setText] = useState("");
   const [items, setItems] = useState([]);
+  useEffect(() => {
+    const savedItems = localStorage.getItem("todos");
+    if (savedItems) {
+      setItems(JSON.parse(savedItems));
+    }
+  }, []);
+  useEffect(() => {
+    if (items.length > 0) {
+      localStorage.setItem("todos", JSON.stringify(items));
+    }
+  }, [items]);
 
   function handleChange(event) {
     setText(event.target.value);
@@ -13,17 +24,44 @@ function App() {
 
   function addItem(event) {
     event.preventDefault();
-    if (text !== "") {
-      setItems((prevItems) => [...prevItems, { text: text }]);
+    if (text !== ""){
+      const newItem = { text: text };
+      const updatedItems = [...items, newItem];
+      setItems(updatedItems);
       setText("");
     }
   }
 
   function deleteItem(id) {
-    setItems((prevItems) => prevItems.filter((item, index) => index !== id));
+    const updatedItems = items.filter((item, index) => index !== id);
+    setItems(updatedItems);
   }
+  const [daysDifference, setDaysDifference] = useState(null);
+
+  useEffect(() => {
+    const storedDateString = localStorage.getItem('storedDate');
+    const currentDate = new Date();
+    
+    if (storedDateString) {
+      const storedDate = new Date(storedDateString);
+      if (storedDate.getTime() !== currentDate.getTime()) {
+        const timeDifference = currentDate - storedDate;
+        const daysDifference = Math.floor(timeDifference / (1000 * 3600 * 24));
+        setDaysDifference(Math.max(daysDifference,1));
+        localStorage.setItem('storedDate', currentDate.toISOString());
+      } else {
+        setDaysDifference(1);
+      }
+    } else {
+      localStorage.setItem('storedDate', currentDate.toISOString());
+      setDaysDifference(1);
+    }
+  }, []);
+
 
   return (
+    <>
+    <header><h2 id="streak"> {daysDifference} 🔥</h2></header>
     <div id="application">
       <div id="todo">
         <h1>To-Do!!</h1>
@@ -31,7 +69,9 @@ function App() {
       <div>
         <form onSubmit={addItem}>
           <input onChange={handleChange} type="text" value={text} />
-          <button type="submit"><AddIcon/></button>
+          <button type="submit">
+            <AddIcon />
+          </button>
         </form>
       </div>
       <div id="content">
@@ -40,6 +80,7 @@ function App() {
         ))}
       </div>
     </div>
+    </>
   );
 }
 
